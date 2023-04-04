159b902184fc2e62f61325fabc7c6f64814d0717
Yuri-Gabutero24 <124618894+Yuri-Gabutero24@users.noreply.github.com>
2023-04-04 16:24:35 +0000

    webpage

--- js/script.js
+++ js/script.js
@@ -1,5 +1,25 @@
 // Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: Mr. Coxall
-// Created on: Sep 2020
+// Created by: Yuri G
+// Created on: April 2023
 // This file contains the JS functions for index.html
+
+
+'use strict'
+/**
+* This function is for calculating the volume of a pyramid
+*/
+function calculate() {
+// input
+  const pyramidlength = parseInt(document.getElementById('length-pyramid"').value)
+  const pyramidwidth = parseInt(document.getElementById('width-pyramid').value)
+  const pyramidheight = parseInt(document.getElementById('height-pyramid').value)
+
+
+ // process
+ const volume = (pyramidlength * pyramidwidth * pyramidheight) / 3
+
+
+  // output
+  document.getElementById('volume-pyramid').innerHTML = "The volume is: " + volume + " cm³"
+}
